a5032ee4161f9ef3ebe9c4d9ae8c8ba97ece54b4
pal <danyi.pali06@gmail.com>
2025-12-25 20:42:41 +0100

    fixed checking habits and the tree's growth

--- tracker.js
+++ tracker.js
@@ -7,7 +7,7 @@ const deleteHabitButton = document.getElementById("delete-habit");
 const habitList = document.getElementById("habits");
 const input = document.getElementById("input-field");
 let habitsCounter = 0;
-let checkedHabits = 0;
+//let checkedHabits = 0;
 let habitMultiplier = 1/6;
 
 let exception;
@@ -59,19 +59,23 @@ habitList.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         if(!e.target.classList.contains("checked")){
             e.target.classList.toggle("checked");
-            checkedHabits++;
-            e.target.id = "list" + checkedHabits;
+            //checkedHabits++;
+            //e.target.id = "list" + checkedHabits;
             calculateTree();
             console.log(e.target);
+        }else{
+            e.target.removeAttribute("class");
+            calculateTree(true);
+            e.target.id = "";
         }
     }
     
     if(e.target.id === "delete"){
         habitsCounter--;
 
-        if(e.target.parentElement.classList.contains("checked")){
-            checkedHabits--;
-        }
+        //if(e.target.parentElement.classList.contains("checked")){
+           // checkedHabits--;
+        //}
         calculateTree(true);
 
         e.target.parentElement.remove();
@@ -79,30 +83,28 @@ habitList.addEventListener("click", function(e){
 }, false);
 
 function calculateTree (isDelete){
-    console.log(checkedHabits, habitsCounter);
-    if(checkedHabits > habitsCounter){
-        exception = "All habits checked";
-        throw(exception);
-    }
 
     try{
 
+        if(treeWidth < 10){
+            treeWidth = 10;
+            treeHeight = 50;
+        }
         console.log("multip: " + habitMultiplier);
         if(isDelete){
-            if(habitsCounter <= 2){
-                treeHeight = 50;
-                treeWidth = 10;
-            }else{
+            if(habitsCounter !== 0){
                 treeWidth = treeWidth - (treeWidth*habitMultiplier);
                 treeHeight = treeHeight - (treeHeight*habitMultiplier);
-                console.log(treeWidth, treeHeight);
             }
+
         }else{
             treeWidth = (treeWidth*habitMultiplier) + treeWidth;
             treeHeight = (treeHeight*habitMultiplier) + treeHeight;
         }
         tree.style.width = treeWidth + "px";
         tree.style.height = treeHeight + "px";
+
+        console.log(treeWidth, treeHeight);
     }catch(exception){
         throw(exception);
     }
