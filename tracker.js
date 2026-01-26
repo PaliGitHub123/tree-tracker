e85dd6502bdc4e8e7c77b56342b7dbad1d0c22d5
pal <danyi.pali06@gmail.com>
2026-01-26 21:47:34 +0100

    bugfix: dblclick

--- tracker.js
+++ tracker.js
@@ -110,12 +110,12 @@ habitList.addEventListener("click", function(e){
 }, false);
 
 habitList.addEventListener("dblclick", function(e){
-    if(e.dblClickState){
+    if(dblClickState){
         exception = "Habit already dblclicked!";
         throw new Error(exception);
     } 
     if(e.target.tagName === "LI"){
-        e.dblClickState = true;
+        dblClickState = true;
         const delButton = e.target.lastChild;
         e.target.removeChild(delButton);
         const habitValueBefore = e.target.innerText;
@@ -136,7 +136,7 @@ habitList.addEventListener("dblclick", function(e){
             const parent = updateButton.parentNode;
             parent.innerHTML = input.value;
             parent.appendChild(delButton);
-            e.dblClickState = false;
+            dblClickState = false;
         })
     }
 })
